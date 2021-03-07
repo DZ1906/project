209ac2ac61ddb3ce01a3a01b6a0ac8588248b7ed
DZ <primaveraa1995@gmail.com>
2021-03-07 22:43:50 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 12;
+console.log(number);
+
+const obj = {
+    name: 'Davron',
+    age: 17,
+    isMarried: false
+
+};
+console.log(obj.age);
+
+// alert ('You have aa 18 age');
+
+// const result = confirm ('how old are you')
+// console.log(result);
+
+const answer = prompt('Вам есть 18?', '');
+console.log(answer);
+
+console.log(3 + + '56');
+
+let incr=10,
+    decr=10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(2*4 == 8);
+
+const isChecked = false,
+    isClosed = false;
+
+console.log(isChecked && !isClosed);
+console.log(obj.isMarried);    
